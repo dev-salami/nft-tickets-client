670e672a1f0fa3d205958399f317c0e10c5d2816
Salami Khalil <salamikhalil02@gmail.com>
2024-02-06 20:57:36 +0100

    updated prop name

--- src/app/buy/page.jsx
+++ src/app/buy/page.jsx
@@ -14,8 +14,6 @@ function Page() {
     "0x22c57a199e26230BC53f40C1c3E7C80ffAB21609"
   );
   const { data, isLoading } = useContractRead(contract, "getEvents");
-  console.log(data);
-  // console.log(data);
 
   return (
     <section>
@@ -28,34 +26,22 @@ function Page() {
               <p>There are currently no available event</p>
             ) : (
               <section className="grid gap-6    grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {data.map((event) => (
+                {data.map((ticket) => (
                   <TicketCard
-                    key={event[0]}
+                    key={ticket[0]}
                     type="purchase"
-                    contractAddress={event[0]}
-                    eventImg={event[2]}
-                    name={event[1]}
+                    contractAddress={ticket[0]}
+                    eventImg={ticket[2]}
+                    name={ticket[1]}
                   />
                 ))}
               </section>
             )}
           </div>
         )}
-
-        {/* <TicketCard type="purchase" />
-        <TicketCard type="purchase" />
-        <TicketCard type="purchase" /> <TicketCard type="purchase" />
-        <TicketCard type="purchase" /> <TicketCard type="purchase" />
-        <TicketCard type="purchase" /> */}
       </div>
-      {/* <BuyTicket /> */}
     </section>
   );
 }
 
 export default Page;
-
-//  <section className="container mx-auto px-6 gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-//    <EventCard />
-//    <EventCard /> <EventCard /> <EventCard /> <EventCard /> <EventCard />
-//  </section>;
